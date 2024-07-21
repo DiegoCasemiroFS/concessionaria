87c8a9e7714e7f092829401cac91db96b9239c21
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-20 23:55:27 -0300

    feat: criando service do veiculo

---
+++ src/main/java/br/com/DiegoCasemiroFS/api/service/VeiculoService.java
@@ -0,0 +1,49 @@
+package br.com.DiegoCasemiroFS.api.service;
+
+import br.com.DiegoCasemiroFS.api.entity.Veiculo;
+import br.com.DiegoCasemiroFS.api.repository.VeiculoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class VeiculoService {
+
+    private final VeiculoRepository veiculoRepository;
+
+    public List<Veiculo> listVeiculos(){
+        return veiculoRepository.findAllByOrderByPrecoAsc();
+    }
+
+    public Veiculo findById(Long id){
+        return veiculoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Veículo não encontrado"));
+    }
+
+    public List<Veiculo> findByMarca(String marca){
+        return veiculoRepository.findByMarcaContainingIgnoreCase(marca);
+    }
+
+    public List<Veiculo> findByNome(String nome){
+        return veiculoRepository.findByNomeContainingIgnoreCase(nome);
+    }
+
+    public Veiculo updateVeiculo(Long id, Veiculo veiculo){
+        return veiculoRepository.findById(id)
+                .map(f -> {
+                    veiculoRepository.findById(id);
+                    veiculoRepository.save(veiculo);
+                    return veiculo;
+                }).orElseThrow(() -> new RuntimeException("Veículo não encontrado"));
+    }
+
+    public void deleteVeiculo(Long id){
+        veiculoRepository.findById(id)
+                .map(f -> {
+                    veiculoRepository.delete(f);
+                    return Void.TYPE;
+                }).orElseThrow(() -> new RuntimeException("Veículo não encontrado"));
+    }
+}
