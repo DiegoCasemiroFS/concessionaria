3d5dbc95bb8dab4ab427f53bdd2bafa666765d6c
Diego Casemiro <dcfs1998@gmail.com>
2024-07-21 19:55:51 -0300

    feat: criando classe pedido

---
+++ src/main/java/br/com/DiegoCasemiroFS/api/entity/Pedido.java
@@ -0,0 +1,29 @@
+package br.com.DiegoCasemiroFS.api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "veiculo_id")
+    private Veiculo veiculo;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    private LocalDateTime dataCadastro;
+}
