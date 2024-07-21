77282de49a76d7a45c1e7e008a99952ac02aa3bc
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-20 23:08:30 -0300

    feat: first commit

---
+++ src/test/java/br/com/DiegoCasemiroFS/api/ConcessionariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.DiegoCasemiroFS.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
