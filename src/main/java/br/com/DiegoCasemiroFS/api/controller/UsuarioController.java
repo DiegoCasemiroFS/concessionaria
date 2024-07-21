0247998f0aeb2ad0a6ea5fe4a1e3d933c7cf6e0d
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-20 23:56:20 -0300

    feat: definindo os tipos de metodos do UsuarioController

--- src/main/java/br/com/DiegoCasemiroFS/api/controller/UsuarioController.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/controller/UsuarioController.java
@@ -3,7 +3,7 @@ package br.com.DiegoCasemiroFS.api.controller;
 import br.com.DiegoCasemiroFS.api.entity.Usuario;
 import br.com.DiegoCasemiroFS.api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,22 +13,27 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
+    @GetMapping
     public List<Usuario> findAll(){
         return usuarioService.findAll();
     }
 
+    @GetMapping
     public Usuario findById(Long id){
         return usuarioService.findById(id);
     }
 
+    @PostMapping
     public Usuario createUsuario(Usuario usuario){
         return usuarioService.createUsuario(usuario);
     }
 
+    @PutMapping
     public Usuario updateUsuario(Long id, Usuario usuario){
         return usuarioService.updateUsuario(id, usuario);
     }
 
+    @DeleteMapping
     public void deleteUsuario(Long id){
         usuarioService.deleteUsuario(id);
     }
