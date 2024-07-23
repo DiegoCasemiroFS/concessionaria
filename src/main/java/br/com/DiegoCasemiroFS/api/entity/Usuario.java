363a516d5be1d81013a14ff630313aa551e5dcea
Diego Casemiro <dcfs1998@gmail.com>
2024-07-22 23:37:43 -0300

    feat: adicionando a variavel senha, a lista de pedidos e implementando o obj UserDetails junto com seus metodos

--- src/main/java/br/com/DiegoCasemiroFS/api/entity/Usuario.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/entity/Usuario.java
@@ -6,15 +6,24 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,22 +31,61 @@ public class Usuario {
 
     @Column(length = 100)
     @NotEmpty(message = "Campo nome e obrigatório")
-    @NotNull(message = "Campo nome não pode ser nulo")
     private String nome;
 
     @Column(length = 80)
     @Email(message = "Informe um email valido")
     @NotEmpty(message = "Campo email e obrigatório")
-    @NotNull(message = "Campo email não deve ser nulo")
     private String email;
 
+    @Column(length = 80)
+    @NotEmpty(message = "Campo senha e obrigatório")
+    private String senha;
+
     @Column(length = 14)
-    @CPF(message = "Informe um cpf valido")
+    @CPF(message = "Informe um CPF valido")
     @NotEmpty(message = "Campo CPF e obrigatório")
-    @NotNull(message = "Campo CPF não deve ser nulo")
     private String cpf;
 
     @NotEmpty(message = "Campo perfil e obrigatório")
     @NotNull(message = "Campo perfil não deve ser nulo")
     private Perfil perfil;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Pedido> pedidos = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USUARIO"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
