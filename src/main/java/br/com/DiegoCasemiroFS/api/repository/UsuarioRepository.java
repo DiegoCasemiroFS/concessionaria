155792fd2a77d866f926810360bb959acc2e76a5
Diego Casemiro <dcfs1998@gmail.com>
2024-07-22 23:39:01 -0300

    feat: criando o metodo findByEmail

--- src/main/java/br/com/DiegoCasemiroFS/api/repository/UsuarioRepository.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/repository/UsuarioRepository.java
@@ -2,8 +2,11 @@ package br.com.DiegoCasemiroFS.api.repository;
 
 import br.com.DiegoCasemiroFS.api.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByEmail(String email);
 }
