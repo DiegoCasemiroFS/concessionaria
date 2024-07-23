d7471cbf4df0f7545735ee1dc336967dd0d99dff
Diego Casemiro <dcfs1998@gmail.com>
2024-07-23 17:13:52 -0300

    feat: anotando o cadastra para que só usuarios do tipo ADMIN possam fazer cadastro de veiculos

--- src/main/java/br/com/DiegoCasemiroFS/api/controller/VeiculoController.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/controller/VeiculoController.java
@@ -3,49 +3,51 @@ package br.com.DiegoCasemiroFS.api.controller;
 import br.com.DiegoCasemiroFS.api.entity.Veiculo;
 import br.com.DiegoCasemiroFS.api.service.VeiculoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("v1/api/veiculo")
+@RequestMapping("/veiculo")
 public class VeiculoController {
 
     private final VeiculoService veiculoService;
 
-    @GetMapping("/{findAll}")
+    @GetMapping("/listagem")
     public List<Veiculo> listVeiculos(){
         return veiculoService.listVeiculos();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/id")
     public Veiculo findById(@PathVariable Long id){
         return veiculoService.findById(id);
     }
 
-    @GetMapping("/{marca}")
+    @GetMapping("/marca")
     public List<Veiculo> findByMarca(@RequestBody String marca){
         return veiculoService.findByMarca(marca);
     }
 
-    @GetMapping("/{nome}")
+    @GetMapping("/nome")
     public List<Veiculo> findByNome(@RequestBody String nome){
         return veiculoService.findByNome(nome);
     }
 
     @PostMapping
-    public Veiculo cadastroVeiculo(@RequestBody Veiculo veiculo){
-        return veiculoService.cadastroVeiculo(veiculo);
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public Veiculo cadastra(@RequestBody Veiculo veiculo){
+        return veiculoService.cadastraVeiculo(veiculo);
     }
 
     @PutMapping
-    public Veiculo updateVeiculo(@PathVariable Long id, @RequestBody Veiculo veiculo){
-        return veiculoService.updateVeiculo(id, veiculo);
+    public Veiculo atualiza(@PathVariable Long id, @RequestBody Veiculo veiculo){
+        return veiculoService.atualizaVeiculo(id, veiculo);
     }
 
     @DeleteMapping
-    public void deleteVeiculo(@PathVariable Long id){
-        veiculoService.deleteVeiculo(id);
+    public void deleta(@PathVariable Long id){
+        veiculoService.deletaVeiculo(id);
     }
 }
