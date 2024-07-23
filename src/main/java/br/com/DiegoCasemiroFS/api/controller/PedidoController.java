22f8b27a262bcc151243b7d96fa4ad3f4af57af6
Diego Casemiro <dcfs1998@gmail.com>
2024-07-23 17:17:20 -0300

    feat: trocando nome dos metodos para padrão portugues

--- src/main/java/br/com/DiegoCasemiroFS/api/controller/PedidoController.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/controller/PedidoController.java
@@ -7,26 +7,24 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("v1/api/pedido")
+@RequestMapping("/pedido")
 public class PedidoController {
 
     private final PedidoService pedidoService;
 
     @PostMapping
-    public Pedido createPedido(@RequestBody Pedido pedido) {
+    public Pedido cria(@RequestBody Pedido pedido) {
         return pedidoService.createPedido(pedido);
     }
 
     @PutMapping
-    public Pedido updatePedido(@PathVariable Long id ,@RequestBody Pedido pedido) {
+    public Pedido atualiza(@PathVariable Long id , @RequestBody Pedido pedido) {
         return pedidoService.updatePedido(id, pedido);
     }
 
     @DeleteMapping
-    public void deletePedido(@PathVariable Long id) {
+    public void deleta(@PathVariable Long id) {
         pedidoService.deletePedido(id);
     }
 
 }
-
-
