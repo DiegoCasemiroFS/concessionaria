b1790fab678d50d14cfcd9460afeb01c684aa0f8
Diego Casemiro <dcfs1998@gmail.com>
2024-07-23 17:18:26 -0300

    feat: atualizando os metodos

--- src/main/java/br/com/DiegoCasemiroFS/api/controller/UsuarioController.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/controller/UsuarioController.java
@@ -1,34 +1,54 @@
 package br.com.DiegoCasemiroFS.api.controller;
 
 import br.com.DiegoCasemiroFS.api.entity.Usuario;
-import br.com.DiegoCasemiroFS.api.entity.dto.TokenDto;
-import br.com.DiegoCasemiroFS.api.entity.dto.UsuarioDto;
+import br.com.DiegoCasemiroFS.api.entity.dto.RegistroRequestDto;
+import br.com.DiegoCasemiroFS.api.entity.dto.ResponseDto;
+import br.com.DiegoCasemiroFS.api.entity.dto.LoginRequestDto;
+import br.com.DiegoCasemiroFS.api.repository.UsuarioRepository;
 import br.com.DiegoCasemiroFS.api.security.JwtService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("v1/api/usuario")
+@RequestMapping("/usuario")
 public class UsuarioController {
 
-    private final AuthenticationManager manager;
-    private final JwtService jwtService;
+    private final UsuarioRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService service;
 
-    @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody @Valid UsuarioDto usuarioDto) {
-        var authenticationToken = new UsernamePasswordAuthenticationToken(usuarioDto.email(), usuarioDto.senha());
-        var authentication = manager.authenticate(authenticationToken);
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDto body){
+        Usuario usuario = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+        if(passwordEncoder.matches(body.senha(), usuario.getPassword())) {
+            String token = this.service.gerarToken(usuario);
+            return ResponseEntity.ok(new ResponseDto(usuario.getNome(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
-        var tokenJwt = jwtService.gerarToken((Usuario) authentication.getPrincipal());
+    @PostMapping("/registro")
+    public ResponseEntity registro(@RequestBody RegistroRequestDto body){
+        Optional<Usuario> user = this.repository.findByEmail(body.email());
 
-        return ResponseEntity.ok(new TokenDto(tokenJwt));
+        if(user.isEmpty()) {
+            Usuario newUser = new Usuario();
+            newUser.setSenha(passwordEncoder.encode(body.senha()));
+            newUser.setEmail(body.email());
+            newUser.setNome(body.nome());
+            this.repository.save(newUser);
+
+            String token = this.service.gerarToken(newUser);
+            return ResponseEntity.ok(new ResponseDto(newUser.getNome(), token));
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
