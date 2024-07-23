b3b57b51760849a449a31bd2621b251c20d641f6
Diego Casemiro <dcfs1998@gmail.com>
2024-07-22 23:35:01 -0300

    feat: criando os dto's

---
+++ src/main/java/br/com/DiegoCasemiroFS/api/entity/dto/TokenDto.java
@@ -0,0 +1,4 @@
+package br.com.DiegoCasemiroFS.api.entity.dto;
+
+public record TokenDto(String token) {
+}
---
+++ src/main/java/br/com/DiegoCasemiroFS/api/entity/dto/UsuarioDto.java
@@ -0,0 +1,4 @@
+package br.com.DiegoCasemiroFS.api.entity.dto;
+
+public record UsuarioDto (String email, String senha){
+}
