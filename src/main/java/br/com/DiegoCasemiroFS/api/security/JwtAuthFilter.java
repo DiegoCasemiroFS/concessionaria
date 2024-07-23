28eb46aa7cdb93bc347183d42c3108e99b92dba7
Diego Casemiro <dcfs1998@gmail.com>
2024-07-22 23:35:19 -0300

    feat: criando as classes de segurança

---
+++ src/main/java/br/com/DiegoCasemiroFS/api/security/JwtAuthFilter.java
@@ -0,0 +1,50 @@
+package br.com.DiegoCasemiroFS.api.security;
+
+import br.com.DiegoCasemiroFS.api.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        var tokenJwt = recuperarToken(request);
+
+        if (tokenJwt != null) {
+            var subject = jwtService.getSubject(tokenJwt);
+            var usuario = usuarioRepository.findByEmail(subject);
+
+            var authenticantion = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authenticantion);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
