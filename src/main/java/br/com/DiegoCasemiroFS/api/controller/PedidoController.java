9312dd2918701fb35c42db7d4e856465cc1934ca
Diego Casemiro <dcfs1998@gmail.com>
2024-07-24 14:01:34 -0300

    feat: atualizando tipo dos metodos e criando o metodo findById

--- src/main/java/br/com/DiegoCasemiroFS/api/controller/PedidoController.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/controller/PedidoController.java
@@ -1,9 +1,13 @@
 package br.com.DiegoCasemiroFS.api.controller;
 
 import br.com.DiegoCasemiroFS.api.entity.Pedido;
+import br.com.DiegoCasemiroFS.api.entity.dto.PedidoDto;
 import br.com.DiegoCasemiroFS.api.service.PedidoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,19 +16,27 @@ public class PedidoController {
 
     private final PedidoService pedidoService;
 
-    @PostMapping("/cria")
-    public Pedido cria(@RequestBody Pedido pedido) {
-        return pedidoService.createPedido(pedido);
+    @GetMapping("/{id}")
+    public Optional<Pedido> findById(@PathVariable Long id){
+        Optional<Pedido> pedido = pedidoService.findById(id);
+        return pedido;
     }
 
-    @PutMapping(("/{id}"))
-    public Pedido atualiza(@PathVariable Long id , @RequestBody Pedido pedido) {
-        return pedidoService.updatePedido(id, pedido);
+    @PostMapping("/cadastra")
+    public Pedido cadastra(@RequestBody PedidoDto pedido){
+        Pedido createdPedido = pedidoService.cadastraPedido(pedido);
+        return createdPedido;
     }
 
-    @DeleteMapping(("/{id}"))
-    public void deleta(@PathVariable Long id) {
-        pedidoService.deletePedido(id);
+    @PutMapping("/{id}")
+    public Optional<Pedido> atualiza(@PathVariable Long id, @RequestBody Pedido pedido){
+        Optional<Pedido> updatedPedido = pedidoService.atualizaPedido(id, pedido);
+        return updatedPedido;
     }
 
+    @DeleteMapping("/{id}")
+    public void deleta(@PathVariable Long id){
+        pedidoService.deletaPedido(id);
+        System.out.println("Pedido deletado: ID " + id);
+    }
 }
