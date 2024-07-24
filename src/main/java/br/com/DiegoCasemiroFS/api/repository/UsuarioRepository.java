123693aa50c6e51860dd52d62e681c0db6e86930
Diego Casemiro <dcfs1998@gmail.com>
2024-07-23 23:53:20 -0300

    feat: removendo anotação

--- src/main/java/br/com/DiegoCasemiroFS/api/repository/UsuarioRepository.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/repository/UsuarioRepository.java
@@ -2,7 +2,6 @@ package br.com.DiegoCasemiroFS.api.repository;
 
 import br.com.DiegoCasemiroFS.api.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
