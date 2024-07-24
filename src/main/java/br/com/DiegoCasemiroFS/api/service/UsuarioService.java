d616a97141ca7201854cf8e47333b74d46a5d2dd
Diego Casemiro <dcfs1998@gmail.com>
2024-07-24 13:20:50 -0300

    feat: recebendo regras de negocio do controller

--- src/main/java/br/com/DiegoCasemiroFS/api/service/UsuarioService.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/service/UsuarioService.java
@@ -1,22 +1,60 @@
 package br.com.DiegoCasemiroFS.api.service;
 
+import br.com.DiegoCasemiroFS.api.entity.Usuario;
+import br.com.DiegoCasemiroFS.api.entity.dto.RegistroRequestDto;
+import br.com.DiegoCasemiroFS.api.entity.dto.LoginRequestDto;
+import br.com.DiegoCasemiroFS.api.entity.dto.ResponseDto;
 import br.com.DiegoCasemiroFS.api.repository.UsuarioRepository;
+import br.com.DiegoCasemiroFS.api.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.util.Optional;
+
+@Service
 @RequiredArgsConstructor
-public class UsuarioService  implements UserDetailsService {
+public class UsuarioService implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return usuarioRepository.findByEmail(username).orElseThrow(
                 () -> new UsernameNotFoundException(username + " não encontrado")
         );
+    }
+
+    public ResponseDto login(LoginRequestDto body) {
+        Usuario usuario = usuarioRepository.findByEmail(body.getEmail())
+                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+
+        if (passwordEncoder.matches(body.getSenha(), usuario.getPassword())) {
+            String token = jwtService.geraToken(usuario);
+            return new ResponseDto(usuario.getNome(), token, usuario.isAdmin());
+        }
+        throw new RuntimeException("Usuario ou senha incorretos");
+    }
+
+    public ResponseDto registro(RegistroRequestDto body) {
+        Optional<Usuario> user = usuarioRepository.findByEmail(body.getEmail());
+
+        if (user.isEmpty()) {
+            Usuario usuario = new Usuario();
+            usuario.setSenha(passwordEncoder.encode(body.getSenha()));
+            usuario.setEmail(body.getEmail());
+            usuario.setNome(body.getNome());
+            usuario.setAdmin(body.isAdmin());
+            usuarioRepository.save(usuario);
+
+            String token = jwtService.geraToken(usuario);
+            return new ResponseDto(usuario.getNome(), token, usuario.isAdmin());
+        }
+        throw new RuntimeException("Email já cadastrado");
     }
 }
