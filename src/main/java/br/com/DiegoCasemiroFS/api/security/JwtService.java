61bfcc278ea637f173f87eb04f59b3c9c920e2c5
Diego Casemiro <dcfs1998@gmail.com>
2024-07-23 23:48:18 -0300

    feat: atualizando metodo geraToken e validaToken

--- src/main/java/br/com/DiegoCasemiroFS/api/security/JwtService.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/security/JwtService.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +19,13 @@ public class JwtService {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    public String gerarToken(Usuario usuario) {
+    public String geraToken(Usuario usuario) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("Concessionaria")
                     .withSubject(usuario.getEmail())
+                    .withClaim("admin", usuario.isAdmin())
                     .withExpiresAt(dataExpiracao())
                     .sign(algorithm);
         } catch (JWTCreationException exception) {
@@ -34,12 +36,14 @@ public class JwtService {
     public String validaToken(String tokenJwt) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            return JWT.require(algorithm)
-                    .withIssuer("concessionaria")
+            DecodedJWT decodedJWT = JWT.require(algorithm)
+                    .withIssuer("Concessionaria")
                     .build()
-                    .verify(tokenJwt)
-                    .getSubject();
-        } catch (JWTVerificationException exception){
+                    .verify(tokenJwt);
+
+            return decodedJWT.getSubject();
+
+        } catch (JWTVerificationException exception) {
             return null;
         }
     }
