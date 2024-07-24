141d58597315bdd337a11631f79d4d8b5302fb13
Diego Casemiro <dcfs1998@gmail.com>
2024-07-24 13:20:14 -0300

    feat: removendo regras de negocio e passando p service

--- src/main/java/br/com/DiegoCasemiroFS/api/controller/UsuarioController.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/controller/UsuarioController.java
@@ -1,55 +1,32 @@
 package br.com.DiegoCasemiroFS.api.controller;
 
-import br.com.DiegoCasemiroFS.api.entity.Usuario;
 import br.com.DiegoCasemiroFS.api.entity.dto.RegistroRequestDto;
 import br.com.DiegoCasemiroFS.api.entity.dto.ResponseDto;
 import br.com.DiegoCasemiroFS.api.entity.dto.LoginRequestDto;
-import br.com.DiegoCasemiroFS.api.repository.UsuarioRepository;
-import br.com.DiegoCasemiroFS.api.security.JwtService;
+import br.com.DiegoCasemiroFS.api.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/usuario")
 public class UsuarioController {
 
-    private final UsuarioRepository repository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService service;
+    private final UsuarioService usuarioService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody LoginRequestDto body){
-        Usuario usuario = this.repository.findByEmail(body.getEmail()).orElseThrow(() -> new RuntimeException("User not found"));
-        if(passwordEncoder.matches(body.getSenha(), usuario.getPassword())) {
-            String token = this.service.geraToken(usuario);
-            return ResponseEntity.ok(new ResponseDto(usuario.getNome(), token, usuario.isAdmin()));
-        }
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ResponseDto> login(@RequestBody LoginRequestDto body) {
+            ResponseDto response = usuarioService.login(body);
+            return ResponseEntity.ok(response);
     }
 
     @PostMapping("/registro")
-    public ResponseEntity registro(@RequestBody RegistroRequestDto body){
-        Optional<Usuario> user = this.repository.findByEmail(body.getEmail());
-
-        if(user.isEmpty()) {
-            Usuario usuario = new Usuario();
-            usuario.setSenha(passwordEncoder.encode(body.getSenha()));
-            usuario.setEmail(body.getEmail());
-            usuario.setNome(body.getNome());
-            usuario.setAdmin(body.isAdmin());
-            this.repository.save(usuario);
-
-            String token = this.service.geraToken(usuario);
-            return ResponseEntity.ok(new ResponseDto(usuario.getNome(), token, usuario.isAdmin()));
-        }
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<ResponseDto> registro(@RequestBody RegistroRequestDto body) {
+            ResponseDto response = usuarioService.registro(body);
+            return ResponseEntity.ok(response);
     }
 }
