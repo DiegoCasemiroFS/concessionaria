61be7dd442139b82072c02eac9d0bcea89ffa999
Diego Casemiro <dcfs1998@gmail.com>
2024-07-23 23:55:23 -0300

    feat: removendo anotações

--- src/main/java/br/com/DiegoCasemiroFS/api/entity/Veiculo.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/entity/Veiculo.java
@@ -2,8 +2,6 @@ package br.com.DiegoCasemiroFS.api.entity;
 
 import br.com.DiegoCasemiroFS.api.entity.enums.TipoVeiculo;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
