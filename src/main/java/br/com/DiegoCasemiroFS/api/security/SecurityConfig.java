5376452f27951146a19b16390ec36ea7163b1163
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-25 14:26:43 -0300

    feat: adicionando os links da documentação no filtro

--- src/main/java/br/com/DiegoCasemiroFS/api/security/SecurityConfig.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         .requestMatchers("/veiculo/*").hasRole("ADMIN")
                         .requestMatchers("/pedido/*").permitAll()
                         .requestMatchers("/vitrine/*").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
