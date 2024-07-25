32a33e630b96baa5d7b488a634d0d4a9d416ab47
DiegoCasemiroFS <dcfs1998@gmail.com>
2024-07-25 18:43:33 -0300

    feat: atualizando nome dos metodos

--- src/main/java/br/com/DiegoCasemiroFS/api/controller/PedidoController.java
+++ src/main/java/br/com/DiegoCasemiroFS/api/controller/PedidoController.java
@@ -1,6 +1,5 @@
 package br.com.DiegoCasemiroFS.api.controller;
 
-import br.com.DiegoCasemiroFS.api.entity.Pedido;
 import br.com.DiegoCasemiroFS.api.entity.dto.PedidoRequestDto;
 import br.com.DiegoCasemiroFS.api.entity.dto.PedidoResponseDto;
 import br.com.DiegoCasemiroFS.api.service.PedidoService;
@@ -22,19 +21,19 @@ public class PedidoController {
     }
 
     @PostMapping("/cadastra")
-    public ResponseEntity<PedidoResponseDto> cadastra(@RequestBody PedidoRequestDto pedidoDto){
+    public ResponseEntity<PedidoResponseDto> cadastraPedido(@RequestBody PedidoRequestDto pedidoDto){
         PedidoResponseDto responseDto = pedidoService.cadastraPedido(pedidoDto);
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PedidoResponseDto> atualiza(@PathVariable Long id, @RequestBody PedidoRequestDto pedidoDto) {
+    public ResponseEntity<PedidoResponseDto> atualizaPedido(@PathVariable Long id, @RequestBody PedidoRequestDto pedidoDto) {
         PedidoResponseDto updatedPedido = pedidoService.atualizaPedido(id, pedidoDto);
         return ResponseEntity.ok(updatedPedido);
     }
 
     @DeleteMapping("/{id}")
-    public void deleta(@PathVariable Long id){
+    public void deletaPedido(@PathVariable Long id){
         pedidoService.deletaPedido(id);
     }
 }
